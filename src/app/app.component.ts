f8fdf2d1cc24b977e2dba499104d2de76ce312c7
arthur ming <mingguobin@live.com>
2019-01-18 16:07:55 +0800

    feat: 添加动态修改页面标题功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'daming-core-ui';
+export class AppComponent implements OnInit {
+
+  title = 'damingerdai-ui';
+
+  constructor(
+    private titleService: Title,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map((route) => {
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        mergeMap(route => route.data),
+        map(data => data as { title: string })
+      ).subscribe(event => this.titleService.setTitle(event.title || this.title));
+  }
 }
