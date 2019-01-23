7ec01cafb57677fcba4605370d8cd1ece826c75a
明国宾 <mingguobin@live.com>
2019-01-23 20:19:56 +0800

    feat: 添加Base模块

---
+++ src/app/routes/base/base-routing.module.ts
@@ -0,0 +1,100 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Base'
+    },
+    children: [
+      {
+        path: '',
+        redirectTo: 'cards'
+      },
+    //   {
+    //     path: 'cards',
+    //     component: CardsComponent,
+    //     data: {
+    //       title: 'Cards'
+    //     }
+    //   },
+    //   {
+    //     path: 'forms',
+    //     component: FormsComponent,
+    //     data: {
+    //       title: 'Forms'
+    //     }
+    //   },
+    //   {
+    //     path: 'switches',
+    //     component: SwitchesComponent,
+    //     data: {
+    //       title: 'Switches'
+    //     }
+    //   },
+    //   {
+    //     path: 'tables',
+    //     component: TablesComponent,
+    //     data: {
+    //       title: 'Tables'
+    //     }
+    //   },
+    //   {
+    //     path: 'tabs',
+    //     component: TabsComponent,
+    //     data: {
+    //       title: 'Tabs'
+    //     }
+    //   },
+    //   {
+    //     path: 'carousels',
+    //     component: CarouselsComponent,
+    //     data: {
+    //       title: 'Carousels'
+    //     }
+    //   },
+    //   {
+    //     path: 'collapses',
+    //     component: CollapsesComponent,
+    //     data: {
+    //       title: 'Collapses'
+    //     }
+    //   },
+    //   {
+    //     path: 'paginations',
+    //     component: PaginationsComponent,
+    //     data: {
+    //       title: 'Pagination'
+    //     }
+    //   },
+    //   {
+    //     path: 'popovers',
+    //     component: PopoversComponent,
+    //     data: {
+    //       title: 'Popover'
+    //     }
+    //   },
+    //   {
+    //     path: 'progress',
+    //     component: ProgressComponent,
+    //     data: {
+    //       title: 'Progress'
+    //     }
+    //   },
+    //   {
+    //     path: 'tooltips',
+    //     component: TooltipsComponent,
+    //     data: {
+    //       title: 'Tooltips'
+    //     }
+    //   }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BaseRoutingModule {}
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -7,9 +7,8 @@ export const routes = [
         component: LayoutComponent,
         children: [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'theme', loadChildren: './theme/theme.module#ThemeModule' , data: { title: '主页'}},
-            // { path: 'forms', loadChildren: './forms/forms.module#FormsModule', data: { title: '表单'} },
-            // { path: 'table', loadChildren: './table/table.module#TableModule', data: { title: '表格'} },
+            { path: 'theme', loadChildren: './theme/theme.module#ThemeModule'},
+            { path: 'base', loadChildren: './base/base.module#BaseModule'},
         ]
     },
 
