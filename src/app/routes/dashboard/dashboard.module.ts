0b9d4e47062e3bd30f6c6c98b8a4cab681486333
明国宾 <mingguobin@live.com>
2019-01-24 21:55:56 +0800

    build: 添加腻子以便支持更多浏览器

--- src/app/routes/dashboard/dashboard.module.ts
+++ src/app/routes/dashboard/dashboard.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent],
   imports: [
-    CommonModule
+    SharedModule,
   ]
 })
 export class DashboardModule { }
---
+++ src/app/routes/dashboard/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
