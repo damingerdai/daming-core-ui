f7d00d83c676262f22b7aa4fbf77f0f858a53e73
arthur ming <mingguobin@live.com>
2019-01-25 13:53:42 +0800

    feat: 完成dropdowns部分

--- src/app/routes/buttons/buttons-routing.module.ts
+++ src/app/routes/buttons/buttons-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { BrandButtonsComponent } from './brand-buttons/brand-buttons.component';
+import { DropdownsComponent } from './dropdowns/dropdowns.component';
 
 
 const routes: Routes = [
@@ -22,13 +23,13 @@ const routes: Routes = [
           title: 'Buttons'
         }
       },
-     // {
-      //   path: 'dropdowns',
-      //   component: DropdownsComponent,
-      //   data: {
-      //     title: 'Dropdowns'
-      //   }
-      // },
+     {
+        path: 'dropdowns',
+        component: DropdownsComponent,
+        data: {
+          title: 'Dropdowns'
+        }
+      },
       {
         path: 'brand-buttons',
         component: BrandButtonsComponent,
--- src/app/routes/buttons/buttons.module.ts
+++ src/app/routes/buttons/buttons.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { ButtonsRoutingModule } from './buttons-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { BrandButtonsComponent } from './brand-buttons/brand-buttons.component';
+import { DropdownsComponent } from './dropdowns/dropdowns.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
     ButtonsComponent,
     BrandButtonsComponent,
+    DropdownsComponent,
   ],
   imports: [
+    SharedModule,
     ButtonsRoutingModule
   ]
 })
