4e5ad809c894d26a0f8296953d238dbe24f3d82f
Arthur Ming <guobin.ming@pwc.com>
2021-11-09 11:33:28 +0800

    fix: fix the dependencies confilct

--- src/app/routes/nav.ts
+++ src/app/routes/nav.ts
@@ -251,20 +251,5 @@ export const navItems: NavData[] = [
 			text: 'NEW'
 		},
 		attributes: { disabled: true },
-	},
-	{
-		name: 'Download CoreUI',
-		url: 'http://coreui.io/angular/',
-		icon: 'icon-cloud-download',
-		class: 'mt-auto',
-		variant: 'success',
-		attributes: { target: '_blank', rel: 'noopener' }
-	},
-	{
-		name: 'Try CoreUI PRO',
-		url: 'http://coreui.io/pro/angular/',
-		icon: 'icon-layers',
-		variant: 'danger',
-		attributes: { target: '_blank', rel: 'noopener' }
 	}
 ];
